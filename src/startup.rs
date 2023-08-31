dce89a29a44087d3816fa7773651c7a0b39a9ae8
Vladimir Ćirković <wladimir.cirkovic@gmail.com>
2023-08-31 21:55:41 +0200

    cargo fmt

--- src/startup.rs
+++ src/startup.rs
@@ -1,7 +1,7 @@
 use std::net::TcpListener;
 
-use actix_web::{App, HttpServer, web};
 use actix_web::dev::Server;
+use actix_web::{web, App, HttpServer};
 
 use crate::routes::{health_check, subscribe};
 
@@ -11,7 +11,7 @@ pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
     })
-        .listen(listener)?
-        .run();
+    .listen(listener)?
+    .run();
     Ok(server)
 }
