6c7dd7207f78ce44e2b142059370a845cdd7e3da
Vladimir Ćirković <wladimir.cirkovic@gmail.com>
2023-09-01 18:50:56 +0200

    updated actions, added conf

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use std::net::TcpListener;
+use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    // Panic if we can't read configuration
+    let configuration = get_configuration().expect("Failed to read configuration.");
+
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind");
     run(listener)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,7 @@
 //! tests/health_check.rs
+use sqlx::{Connection, PgConnection};
 use std::net::TcpListener;
+use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
 fn spawn_app() -> String {
@@ -28,6 +30,11 @@ async fn health_check_works() {
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_json_data() {
     let address = spawn_app();
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_string = configuration.database.connection_string();
+    let mut connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to Postgres.");
     let client = reqwest::Client::new();
 
     let body = "{\"name\":\"my-name\", \"email\": \"a@b.com\"}";
@@ -39,7 +46,13 @@ async fn subscribe_returns_a_200_for_valid_json_data() {
         .await
         .expect("Failed to send request");
 
-    assert_eq!(200, response.status().as_u16())
+    assert_eq!(200, response.status().as_u16());
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscription.");
+    assert_eq!(saved.email, "a@b.com");
+    assert_eq!(saved.name, "my-name");
 }
 
 #[tokio::test]
